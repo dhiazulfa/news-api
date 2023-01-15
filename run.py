89826b130c47073c6d345ca0d452f4d0d8bec1f7
Dhiazulfa <42112021+dhiazulfa@users.noreply.github.com>
2023-01-15 13:18:46 +0700

    update
    menambah filter /news/today

--- run.py
+++ run.py
@@ -2,10 +2,25 @@ from elasticsearch_dsl import Search, Q
 from flask import Flask, jsonify, request
 from elasticsearch import Elasticsearch
 
+from datetime import datetime, timedelta
+
 app = Flask(__name__)
 
 # connect to Elasticsearch
-es = Elasticsearch(["http://localhost:9200/"])
+es = Elasticsearch(["http://103.157.96.203:9200/"])
+
+#ambil data news hari ini
+@app.route("/news/today", methods=["GET"])
+def get_today_news():
+    page = request.args.get("page", default=1, type=int)
+    per_page = 12
+    start = (page - 1) * per_page
+    end = start + per_page
+
+    today = datetime.now().date()
+    s = Search(using=es, index="news_indonesia").filter("range", tanggal={"gte": today, "lte": today + timedelta(days=1)})[start:end]
+    response = s.execute()
+    return jsonify([hit.to_dict() for hit in response])
 
 # ambil semua berita dengan paginasi per 10 berita
 @app.route("/news", methods=["GET"])
